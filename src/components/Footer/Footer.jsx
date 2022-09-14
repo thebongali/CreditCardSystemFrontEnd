ef522fd259a67c193f0c408c480aaf371167dc54
thebongali <ghosh.subhankar619@gmail.com>
2022-09-14 10:38:14 +0530

    Master commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../Footer/Footer.css";
+
+function Footer() {
+  return (
+    <React.Fragment>
+        <footer>
+            <span>All rights reserved</span>
+            <span>@ Batch 3 Group 5</span>
+        </footer>
+    </React.Fragment>
+  )
+}
+
+export default Footer
